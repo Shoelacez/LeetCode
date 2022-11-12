3538d4434e906a2c065d4c46e2f81255e8e87040
Wicca <92827559+Shoelacez@users.noreply.github.com>
2022-11-12 15:15:49 +0800

    Accepted Leetcode solution
    I will need to do some optimization to my code but as a beginner this is more than enough to just meet the test cases. BABY STEPS.
    12.11.2022 saturday, China, shenyang aerospace university

--- Difficulty/Easy/palindrome number.py
+++ Difficulty/Easy/palindrome number.py
@@ -1,32 +1,27 @@
-x = -121
-original_x = x
-output = True
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        original_x = x
+        # split the number into individual nums
+        indiv_nums = []
+        digit_count = -1
+        while x > 0:
+            rem = x % 10
+            indiv_nums.append(rem)
+            x = x // 10
+            digit_count += 1
 
-# split the number into individual nums
-indiv_nums = []
-digit_count = -1
-while x > 0:
-    rem = x % 10
-    indiv_nums.append(rem)
-    x = x // 10
-    digit_count += 1
+        # Reverse the numbers
+        iteration = 0
+        length = len(indiv_nums)
 
-print(indiv_nums)
+        reversed_number = 0
+        while iteration != length:
+            reversed_number += indiv_nums[iteration] * 10 ** digit_count
+            iteration += 1
+            digit_count -= 1
 
-# Reverse the numbers
-iteration = 0
-length = len(indiv_nums)
-
-reversed_number = 0
-while iteration != length:
-    reversed_number += indiv_nums[iteration] * 10 ** digit_count
-    iteration += 1
-    digit_count -= 1
-
-print(reversed_number)
-
-# Compare the reversed number to x, if same: True else False
-if reversed_number == original_x:
-    print("True")
-else:
-    print("False")
+        # Compare the reversed number to x, if same: True else False
+        if reversed_number == original_x:
+            return True
+        else:
+            return False
