9969119fd66978ba3cc3ec2fcd9038b30a9627c5
Wicca <92827559+Shoelacez@users.noreply.github.com>
2022-11-12 15:07:49 +0800

    Update addbinary.py

--- Difficulty/Easy/addbinary.py
+++ Difficulty/Easy/addbinary.py
@@ -4,8 +4,8 @@ b = "1011"
 
 def convert_to_dec(binary):
     decimal_num = 0
-    for i, c in enumerate(binary):
-        digit = int(c)
+    for i, char in enumerate(binary):
+        digit = int(char)
         r = digit * 2 ** ((len(binary) - 1) - i)
         decimal_num += r
     return decimal_num
@@ -17,4 +17,3 @@ dec_b = convert_to_dec(b)
 add_dec = dec_b + dec_a
 add_bin = bin(add_dec)
 print(add_bin[2:])
-
