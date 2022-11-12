e3d5f908140453887f27d35d615a9c1efcd19d32
Wicca <92827559+Shoelacez@users.noreply.github.com>
2022-11-12 14:52:09 +0800

    Matching the leetcode boiler plate
    I just think it is sometimes pointless writing code that only you can understand so i prefer it being longr but almost anyone can understand what is going on.

--- Difficulty/Easy/plus one.py
+++ Difficulty/Easy/plus one.py
@@ -1,39 +1,26 @@
-digits = [9, 9, 9]
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:     
+        # 1. Construct an integer from the list
+        number = 0
+        for i in range(len(digits)):
+            r = digits[i] * 10 ** (len(digits) - (1 + i))
+            number += r
 
-'''APPROACH 01'''
-# last_num = digits[len(digits) - 1]
-# last_num += 1
-#
-# digits[len(digits) - 1] = last_num
-#
-# print(digits)
+        # 2. Increment the number by 1
+        number += 1
+        og_num = number
 
-'''APPROACH 02'''
-# 1. Construct an integer from the list
-number = 0
-for i in range(len(digits)):
-    r = digits[i] * 10 ** (len(digits) - (1 + i))
-    number += r
+        # 3. Reconstructing back the list
+        output = []
+        while number > 0:
+            rem = number % 10
+            output.append(rem)
+            number = number // 10
 
-# print(number)
+        # 4. Reversing the number
+        correct_order = []
+        for i,n in enumerate(output):
+            r = output[(len(output)-1)-i]
+            correct_order.append(r)
 
-# 2. Increment the number by 1
-number += 1
-og_num = number
-
-# 3. Reconstructing back the list
-output = []
-while number > 0:
-    # print(number)
-    rem = number % 10
-    output.append(rem)
-    number = number // 10
-
-
-# 4. Reversing the number
-correct_order = []
-for i,n in enumerate(output):
-    r = output[(len(output)-1)-i]
-    correct_order.append(r)
-
-print(correct_order)
+        return correct_order
