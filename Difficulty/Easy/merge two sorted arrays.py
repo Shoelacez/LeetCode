2e36ae033e41d6524a0aca1f13cac718fd56cfd2
Wicca <92827559+Shoelacez@users.noreply.github.com>
2022-11-12 16:49:23 +0800

    Create merge two sorted arrays.py

---
+++ Difficulty/Easy/merge two sorted arrays.py
@@ -0,0 +1,11 @@
+list1 = [ ]
+list2 = [0]
+
+expected_output = [1, 1, 2, 3, 4, 4]
+output = []
+
+for item1,item2 in zip(list1,list2):
+    output.append(item1)
+    output.append(item2)
+
+print(output)
