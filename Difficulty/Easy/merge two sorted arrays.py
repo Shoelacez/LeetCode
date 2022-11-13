19f199c69767d27a6b6d0a069e2a9cffe8db656a
Wicca <92827559+Shoelacez@users.noreply.github.com>
2022-11-13 20:57:18 +0800

    implementation of lists
    Got to learn about Singly linked lists on python

--- Difficulty/Easy/merge two sorted arrays.py
+++ Difficulty/Easy/merge two sorted arrays.py
@@ -9,3 +9,33 @@ for item1, item2 in zip(list1, list2):
     output.append(item2)
 
 print(output)
+
+
+'''Gotta learn about Singly linked lists on python'''
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+# Link first Node to second node
+list.headval.nextval = e2
+
+# Link second Node to third node
+e2.nextval = e3
+
+list.listprint()
