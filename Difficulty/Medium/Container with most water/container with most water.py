685c6f3751ac580d72786722e89d02be74659f6b
Wicca <92827559+Shoelacez@users.noreply.github.com>
2022-11-15 18:06:40 +0800

    Correcting the code
    i had to learn from someone who knew better than me than letting my ego get the best of me.

---
+++ Difficulty/Medium/Container with most water/container with most water.py
@@ -0,0 +1,24 @@
+'''CORRECT SOLUTION'''
+'''MY OWN SOLUTION THAT WAS WROMG'''
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+#         areas = []
+#         for x, h in enumerate(height):
+#             for _x, _h in enumerate(height):
+#                 # Calculate the area btn them
+#                 if height[x] < height[_x]:
+#                     smallest = height[x]
+#                 else:
+#                     smallest = height[_x]
+#
+#                 l = abs((x - _x))
+#                 area = l* smallest
+#                 # print(area)
+#                 areas.append(area)
+#
+#         uniques = []
+#         for a in areas:
+#             if a not in uniques:
+#                 uniques.append(a)
+#
+#         return max(uniques)
