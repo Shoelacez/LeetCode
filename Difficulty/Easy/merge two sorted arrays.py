fa10b284e48f2a7ded15bb6493ff6bb168ed2183
Wicca <92827559+Shoelacez@users.noreply.github.com>
2022-11-16 18:34:17 +0800

    minor

--- Difficulty/Easy/merge two sorted arrays.py
+++ Difficulty/Easy/merge two sorted arrays.py
@@ -10,22 +10,25 @@ for item1, item2 in zip(list1, list2):
 
 print(output)
 
-
 '''Gotta learn about Singly linked lists on python'''
+
+
 class Node:
-   def __init__(self, dataval=None):
-      self.dataval = dataval
-      self.nextval = None
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
+
 
 class SLinkedList:
-   def __init__(self):
-      self.headval = None
+    def __init__(self):
+        self.headval = None
 
-   def listprint(self):
-      printval = self.headval
-      while printval is not None:
-         print (printval.dataval)
-         printval = printval.nextval
+    def listprint(self):
+        printval = self.headval
+        while printval is not None:
+            print(printval.dataval)
+            printval = printval.nextval
+
 
 list = SLinkedList()
 list.headval = Node("Mon")
@@ -39,3 +42,44 @@ list.headval.nextval = e2
 e2.nextval = e3
 
 list.listprint()
+
+'''03'''
+# Python program to merge two sorted arrays/
+def mergeArrays(arr1, arr2, n1, n2, arr3):
+	i = 0
+	j = 0
+	k = 0
+
+	# traverse the arr1 and insert its element in arr3
+	while(i < n1):
+		arr3[k] = arr1[i]
+		k += 1
+		i += 1
+
+	# now traverse arr2 and insert in arr3
+	while(j < n2):
+		arr3[k] = arr2[j]
+		k += 1
+		j += 1
+
+	# sort the whole array arr3
+	arr3.sort()
+
+
+# Driver code
+if __name__ == '__main__':
+	arr1 = [1, 3, 5, 7]
+	n1 = len(arr1)
+
+	arr2 = [2, 4, 6, 8]
+	n2 = len(arr2)
+
+	arr3 = [0 for i in range(n1+n2)]
+	mergeArrays(arr1, arr2, n1, n2, arr3)
+
+	print("Array after merging")
+	for i in range(n1 + n2):
+		print(arr3[i], end=" ")
+
+# This code is contributed by Tapesh(tapeshdua420)
+
