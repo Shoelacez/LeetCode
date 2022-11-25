cf80c8f43f45d1900fab8f55e2424d142a0ab177
Wicca <92827559+Shoelacez@users.noreply.github.com>
2022-11-25 18:24:53 +0800

    minor tweaks

--- Difficulty/Easy/Remove Element/remove element.py
+++ Difficulty/Easy/Remove Element/remove element.py
@@ -2,16 +2,6 @@
 #  打印02 and i have no clue on wg=hat is going on.
 
 # Initialization
-counter = 1
-num = 1
-total  = 0
-
-# Reading the numbers from the user
-while num > 0:
-    num = int(input(f"Num {counter}: "))
-    counter += 1
-    total += num
-
-print(total - num)
+# I had to change stufs
 
 
