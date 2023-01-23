31a2bc2fe51e5daadabe78160127ef4c49f80e30
Wicca <alphayojoseph1738@gmail.com>
2023-01-23 12:28:28 +0800

    remove element problem correction

--- Difficulty/Easy/Remove Element/remove element.py
+++ Difficulty/Easy/Remove Element/remove element.py
@@ -1,14 +1,13 @@
-nums = [0,1,2,2,3,0,4,2]
-val = 2
-expected_output = 2
-# nums = [2, 2, 0, 0]
+nums = [3, 2, 2, 3, 5]
+val = 3
 
-result = []
-count = 0
-for num in nums:
-    if val != num:
-        result.append(num)
-        count += 1
-    
-print(result)
-print(count)
+
+counter = 0
+for i in range(len(nums)):
+    if nums[i] != val:
+        nums[counter] = nums[i]
+        counter += 1
+        print(counter)
+
+print(nums)
+
