759466bbc5f1af8de233c432bc95f59853a1ddcc
Wicca <92827559+Shoelacez@users.noreply.github.com>
2023-02-06 11:33:18 +0800

    Remove duplicatae (done in Yiwu)

--- Difficulty/Easy/Remove duplicate from sorted list/remove duplicate.py
+++ Difficulty/Easy/Remove duplicate from sorted list/remove duplicate.py
@@ -5,16 +5,54 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        h = self.
+    def __init__(self):
+        self.head = None
+
+    def insert(self, num):
+        new_node = ListNode(num)        
+        new_node.next = self.head
+        self.head = new_node
+
+    # def traversal(self):
+    #     h = self.head
+
+    #     if h is None:
+    #         print(f"Linked List is empty")
+        
+    #     else:
+    #         while h is not None:
+    #             print(f"{h.val}->", end="")
+    #             h = h.next
+
+
+    def deleteDuplicates(self):
+        h = self.head
         if h is None:
-            return
+            return f"List empty"
         else:
+            prev_val = 0
             while h is not None:
-                prev_val = 0
                 if h.val == prev_val:
-                    # delete
-                    h = h.next
+                    print(f"{h.val} is a Duplicate")
                 else:
-                    prev_val = h.val
+                    print(f"{h.val}->")
+                    # delete h.val
+                prev_val = h.val
                 h = h.next
+
+
+list1 = [1,2,3,4,4,6,22]
+soln = Solution()
+
+soln.insert(1)
+soln.insert(2)
+soln.insert(3)
+soln.insert(4)
+soln.insert(4)
+soln.insert(6)
+soln.insert(22)
+
+
+soln.deleteDuplicates()
+
+# soln.traversal()
