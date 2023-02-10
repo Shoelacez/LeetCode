632840f5c8430f302a995b66d7fc328277b32b72
Wicca <92827559+Shoelacez@users.noreply.github.com>
2023-02-10 17:08:58 +0800

    Updated

--- Difficulty/Easy/Remove duplicate from sorted list/remove duplicate.py
+++ Difficulty/Easy/Remove duplicate from sorted list/remove duplicate.py
@@ -1,58 +1,18 @@
-# Definition for singly-linked list.
+Definition for singly-linked list.
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
-    def __init__(self):
-        self.head = None
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        curr = head
+        while curr.next:
+            if curr.val == curr.next.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return head
 
-    def insert(self, num):
-        new_node = ListNode(num)        
-        new_node.next = self.head
-        self.head = new_node
-
-    # def traversal(self):
-    #     h = self.head
-
-    #     if h is None:
-    #         print(f"Linked List is empty")
-        
-    #     else:
-    #         while h is not None:
-    #             print(f"{h.val}->", end="")
-    #             h = h.next
-
-
-    def deleteDuplicates(self):
-        h = self.head
-        if h is None:
-            return f"List empty"
-        else:
-            prev_val = 0
-            while h is not None:
-                if h.val == prev_val:
-                    print(f"{h.val} is a Duplicate")
-                else:
-                    print(f"{h.val}->")
-                    # delete h.val
-                prev_val = h.val
-                h = h.next
-
-
-list1 = [1,2,3,4,4,6,22]
-soln = Solution()
-
-soln.insert(1)
-soln.insert(2)
-soln.insert(3)
-soln.insert(4)
-soln.insert(4)
-soln.insert(6)
-soln.insert(22)
-
-
-soln.deleteDuplicates()
-
-# soln.traversal()
