cf80073674e8f25870aabf3a786a190d1328cefd
Wicca <92827559+Shoelacez@users.noreply.github.com>
2023-02-11 11:30:33 +0800

    Merge a sorted array

--- Difficulty/Easy/Merge two sorted arrays/merge two sorted arrays.py
+++ Difficulty/Easy/Merge two sorted arrays/merge two sorted arrays.py
@@ -1,31 +1,47 @@
-# Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, data=0, ref=None):
+class Node:
+    def __init__(self, data):
         self.data = data
-        self.ref = ref
+        self.ref = None
+
+class LinkedList:
+    list1 = []
+    list2 = []
+
+    def __init__(self):
+        self.head = None
+
+    def printList(self):
+        h = self.head
+        if h is None:
+            print("LL is empty")
+
+        else:
+            while h is not None:
+                print(f"{h.data}->", end="")
+                self.list1.append(h.data)
+                h = h.ref
+
+    def insert(self, num):
+        # create a node
+        new_node = Node(num)
+        # connect the new_node  to the head
+        new_node.ref = self.head
+        # set the new_node as the head
+        self.head = new_node
+
+    def merge(self, l1, l2):
+        pass
+
+    def createL1(self):
+        selection = "#"
+        while not selection:
+            input("Num: ")
 
 
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        dummy = ListNode()
-        head = dummy
+ll = LinkedList()
+# ll.insert(3)
+# ll.insert(2)
+# ll.insert(1)
+ll.createL1()
 
-        # if one of the list is empty, take the remainig nums into the output
-        while l1 and l2:
-            if l1.data < l2.data:
-                head.ref = l1
-                l1 = l1.ref
-            else:
-                head.ref = l2
-                l2 = l2.ref
-            head = head.ref
-
-        if l1:
-            # take the remaining portion and insert it at the end of the list
-            head.ref = l1
-        elif l2:
-            head.ref = l2
-
-        return dummy.ref
-
-
+ll.printList()
---
+++ Difficulty/Easy/Merge two sorted arrays/problem 02.py
@@ -0,0 +1,5 @@
+list1 = [1, 2, 3]
+list2 = [2, 5, 6]
+
+for i in range(len(list1)):
+    print(list1[i])
